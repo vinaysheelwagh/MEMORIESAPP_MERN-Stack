9b073a36ac27fa8d0b5af023322ce688faf25d64
Vinaysheel Wagh <vinaysheel_wagh@hotmail.com>
2020-12-29 16:00:57 +0000

    api for getting and creating posts
    Created a model for posts. Added a controller and router to handle get and post requests.

--- server/index.js
+++ server/index.js
@@ -3,4 +3,23 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-const app = express
+import postRoutes from "./routes/posts.js";
+
+const app = express()
+
+app.use('/posts',postRoutes)
+
+app.use(bodyParser.json({limit:"30mb",extended: true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended: true}))
+app.use(cors)
+
+//mongoDb connection 
+
+const CONNECTION_URL = 'mongodb+srv://vin19:vin19@cluster0.7darx.mongodb.net/memories?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT, console.log(`Server running on port: ${PORT}`)))
+    .catch((error)=> console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
