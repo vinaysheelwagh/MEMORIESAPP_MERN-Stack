a17cc081fa2b0cded697b1d3a15886f59edff734
Vinaysheel Wagh <vinaysheel_wagh@hotmail.com>
2021-01-02 16:56:03 +0000

    configuration and environment chnages
    Adding environment variables for the server deployment and adding configurations

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-
+import dotenv from 'dotenv'
 import postRoutes from "./routes/posts.js";
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit:"30mb",extended: true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended: true}))
@@ -15,10 +16,9 @@ app.use('/posts',postRoutes)
 
 //mongoDb connection 
 
-const CONNECTION_URL = 'mongodb+srv://vin19:vin19@cluster0.7darx.mongodb.net/memories?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, console.log(`Server running on port: ${PORT}`)))
     .catch((error)=> console.log(error.message))
 
