db29cf8b415e1f9f5aa37d77102fe4a7ec6d1ef2
Vinaysheel Wagh <vinaysheel_wagh@hotmail.com>
2021-01-02 09:30:48 +0000

    Update & Delete Api
    Api for updating and deleting the memory

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { getPost, createPost } from "../controllers/posts.js";
+import { getPost, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
 
 const router = express.Router()
 
 router.get('/', getPost)
-router.get('/', createPost)
-
+router.post('/', createPost)
+router.patch('/:id',updatePost)
+router.delete('/:id',deletePost)
+router.patch('/:id/likePost',likePost)
 export default router
